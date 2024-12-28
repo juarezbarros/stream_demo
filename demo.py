ce3bff6c0b284ececfd2a63b36a6ac8194f84e1f
juarezbarros <sba24105@student.cct.ie>
2024-12-28 13:07:34 +0000

    5

--- demo.py
+++ demo.py
@@ -1,48 +1,28 @@
-pip install matplotlib
-pip install plotly
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
 
 
-st.title("Dashboard")
+st.title(" Gr")
 
 
-st.sidebar.title("Parametrers")
-combined_df = pd.read_csv('combined_df.csv')
+data = {
+    "Ano": [2018, 2019, 2020, 2021, 2022],
+    "Vendas": [100, 200, 300, 400, 500]
+}
 
 
-areas = combined_df["Area"].unique()
-items = combined_df["Item"].unique()
-
-area_selected = st.sidebar.selectbox("Selecione a Área", areas)
-item_selected = st.sidebar.selectbox("Selecione o Item", items)
+df = pd.DataFrame(data)
 
 
-filtered_df = combined_df[(combined_df["Area"] == area_selected) & 
-                          (combined_df["Item"] == item_selected)]
+st.subheader("Dados de Vendas")
+st.dataframe(df)
 
-
-# Graph1
-st.write("### Graph")
 fig, ax = plt.subplots()
-ax.plot(filtered_df["Year"], filtered_df["Production"], label="Production", marker="o")
-ax.plot(filtered_df["Year"], filtered_df["Predicted_Production"], label="Predicted Production", 
-        linestyle="--", marker="x", color="red")
-ax.set_title(f"Produção de {item_selected} em {area_selected}")
+ax.plot(df["Ano"], df["Vendas"], marker='o', linestyle='-', color='blue')
+ax.set_title("Vendas por Ano")
 ax.set_xlabel("Ano")
-ax.set_ylabel("Produção")
-ax.legend()
-st.pyplot(fig)
+ax.set_ylabel("Vendas")
 
-    # Graph2
-st.write("### Gráfico 2")
-fig_interactive = px.line(
-    filtered_df, 
-    x="Year", 
-    y=["Production", "Predicted_Production"],
-    title=f"Produção de {item_selected} em {area_selected}",
-    labels={"value": "Produção", "variable": "Tipo"}
-)
-st.plotly_chart(fig_interactive)
+
+st.pyplot(fig)

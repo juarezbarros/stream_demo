104b4338079246b990af421f6705f43cc7539f36
juarezbarros <sba24105@student.cct.ie>
2024-12-28 14:45:01 +0000

    9

--- demo.py
+++ demo.py
@@ -3,44 +3,59 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 
+df_dash_eu_total=pd.read_csv("FAOSTAT_data_en_12_5_2024.csv")
+df_dash_eu_total = df_dash_eu_total[df_dash_eu_total['Item'].isin(['Barley', 'Wheat'])]
 
-st.title("Dashboard de Produção Agrícola")
+dash_df = df_dash_eu_total.pivot_table(index=['Area', 'Year', 'Item'], columns='Element', values='Value', aggfunc='sum'
+).sort_values(by='Production', ascending=False).reset_index()
 
-# Sidebar para seleção de parâmetros
-st.sidebar.title("Parâmetros")
-combined_df = pd.read_csv('combined_df.csv')
+dash_df = dash_df.sort_values(by=['Area', 'Item', 'Year'])
 
-# Obter áreas e itens únicos do DataFrame
-areas = combined_df["Area"].unique()
-items = combined_df["Item"].unique()
+st.title("Agriculture Dashboard")
+pivoted_df = dash_df
 
-area_selected = st.sidebar.selectbox("Selecione a Área", areas)
-item_selected = st.sidebar.selectbox("Selecione o Item", items)
 
-# Filtrar dados com base na seleção do usuário
-filtered_df = combined_df[(combined_df["Area"] == area_selected) & 
-                          (combined_df["Item"] == item_selected)]
+st.sidebar.title("Parameters")
+years = pivoted_df["Year"].unique()  # Obter anos únicos
+items = pivoted_df["Item"].unique()  # Obter itens únicos (ex: "Wheat", "Barley")
 
-st.write("### Dados de Produção Filtrados", filtered_df)
+year_selected = st.sidebar.selectbox("Select Year", years)
+item_selected = st.sidebar.selectbox("Select Item", items)
 
-st.write("### Gráfico 1")
-fig, ax = plt.subplots()
-ax.plot(filtered_df["Year"], filtered_df["Production"], label="Production", marker="o")
-ax.plot(filtered_df["Year"], filtered_df["Predicted_Production"], label="Predicted Production", 
-        linestyle="--", marker="x", color="red")
-ax.set_title(f"Produção de {item_selected} em {area_selected}")
-ax.set_xlabel("Ano")
-ax.set_ylabel("Produção")
-ax.legend()
+
+df_filtered = pivoted_df[(pivoted_df["Year"] == year_selected) & (pivoted_df["Item"] == item_selected)]
+
+
+df_filtered = df_filtered.sort_values(by='Production', ascending=False)
+
+
+st.write(f"### Productio Data for the Year {year_selected} and Item {item_selected}", df_filtered)
+
+
+fig, ax1 = plt.subplots(figsize=(10, 6))
+
+
+ax1.bar(df_filtered['Area'], df_filtered['Production'], color='green', label='Production')
+ax1.bar(df_filtered['Area'], df_filtered['Area harvested'], color='darkorange', label='Area harvested')
+ax1.set_xlabel('Country')
+ax1.set_ylabel('Production / Area harvested')
+
+
+ax2 = ax1.twinx()
+ax2.plot(df_filtered['Area'], df_filtered['Yield'], color='red', marker='o', label='Yield', linestyle='--', alpha=0.5)
+ax2.set_ylabel('Yield')
+
+
+plt.xticks(rotation=90, ha='right', fontsize=10)
+
+
+lines1, labels1 = ax1.get_legend_handles_labels()
+lines2, labels2 = ax2.get_legend_handles_labels()
+ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper right')
+
+
+plt.title(f'Production, Area harvested e Yield per Year {year_selected} and Item {item_selected}')
+fig.tight_layout()  
+
+
 st.pyplot(fig)
-
-
-st.write("### Gráfico 2")
-fig_interactive = px.line(
-    filtered_df, 
-    x="Year", 
-    y=["Production", "Predicted_Production"],
-    title=f"Produção de {item_selected} em {area_selected}",
-    labels={"value": "Produção", "variable": "Tipo"}
-)
-st.plotly_chart(fig_interactive)

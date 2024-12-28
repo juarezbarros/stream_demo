4cea6a7c7a3aa2f62b1fbec68edffa0fee5d3001
juarezbarros <sba24105@student.cct.ie>
2024-12-28 13:00:47 +0000

    import4

--- demo.py
+++ demo.py
@@ -22,26 +22,25 @@ filtered_df = combined_df[(combined_df["Area"] == area_selected) &
                           (combined_df["Item"] == item_selected)]
 
 
-
-    # Graph1
-    st.write("### Gráfico 1")
-    fig, ax = plt.subplots()
-    ax.plot(filtered_df["Year"], filtered_df["Production"], label="Production", marker="o")
-    ax.plot(filtered_df["Year"], filtered_df["Predicted_Production"], label="Predicted Production", 
-            linestyle="--", marker="x", color="red")
-    ax.set_title(f"Produção de {item_selected} em {area_selected}")
-    ax.set_xlabel("Ano")
-    ax.set_ylabel("Produção")
-    ax.legend()
-    st.pyplot(fig)
+# Graph1
+st.write("### Graph")
+fig, ax = plt.subplots()
+ax.plot(filtered_df["Year"], filtered_df["Production"], label="Production", marker="o")
+ax.plot(filtered_df["Year"], filtered_df["Predicted_Production"], label="Predicted Production", 
+        linestyle="--", marker="x", color="red")
+ax.set_title(f"Produção de {item_selected} em {area_selected}")
+ax.set_xlabel("Ano")
+ax.set_ylabel("Produção")
+ax.legend()
+st.pyplot(fig)
 
     # Graph2
-    st.write("### Gráfico 2")
-    fig_interactive = px.line(
-        filtered_df, 
-        x="Year", 
-        y=["Production", "Predicted_Production"],
-        title=f"Produção de {item_selected} em {area_selected}",
-        labels={"value": "Produção", "variable": "Tipo"}
-    )
-    st.plotly_chart(fig_interactive)
+st.write("### Gráfico 2")
+fig_interactive = px.line(
+    filtered_df, 
+    x="Year", 
+    y=["Production", "Predicted_Production"],
+    title=f"Produção de {item_selected} em {area_selected}",
+    labels={"value": "Produção", "variable": "Tipo"}
+)
+st.plotly_chart(fig_interactive)
